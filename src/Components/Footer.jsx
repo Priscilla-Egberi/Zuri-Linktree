99c2b312b534e4049058addae9054460af1af3cb
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-11-04 21:53:53 +0100

    FInished styling the Contact Form

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,9 +2,9 @@ import img1 from "../Assets/IMG.jpg"
 export default function Footer(){
     return(
         <>
-        <section className="container ">
+        <section className="container">
         <hr className="mx-md-4" />
-        <div className="d-block d-md-flex justify-content-between align-items-center container py-4 px-md-4">
+        <div className="d-block d-md-flex justify-content-between align-items-center container py-2 px-md-4">
             <p className="h4 mb-3">Zuri<span className="text-danger fs-1">.</span>Internship</p>
             <p className="text-muted my-3" style={{fontSize:"0.8rem"}}>HNG Internship 9 Frontend task</p>
             <img src={img1} alt="" width='110px' className="my-3" />
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,17 +1,31 @@
-export default function Contact(){
-    const name="{name}"
-    return(
-        <section>
-        <p>Contact Me</p>
-        <p>Hi there, contact me to ask me about anything you have in mind.</p>
-        <form>
-            <input type="text" placeholder="Enter your first name" id='first_name' />
-            <input type="text" placeholder="Enter your last name" id='last_name'/>
-            <input type="email" placeholder="yourname@email.com" id='email'/>
-            <textarea name="" placeholder="Send me a message and I'll reply you as soon as possible..." id="" cols="30" rows="10"></textarea>
-            <input type="checkbox" /> <span>You agree to providing your data to {name} who may contact you.</span>
-            <button id="btn__submit">Send message</button>
-        </form>
+export default function Contact() {
+    const name="Priscilla"
+    return (
+        <section className="container container-contact px-3">
+            <p className="h2">Contact Me</p>
+            <p className="mb-4 mt-4">Hi there, contact me to ask me about anything you have in mind.</p>
+            <form>
+                <div className="row mb-3">
+                    <div className="col-12 col-md-6 mb-3 mb-md-0">
+                        <label>First name</label>
+                        <input type="text" placeholder="Enter your first name" id='first_name' className="w-100" />
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <label>Last name</label>
+                        <input type="text" placeholder="Enter your last name" id='last_name' className="w-100" />
+                    </div>
+                </div>
+                <div className="mb-3">
+                    <label>Email</label>
+                    <input type="email" placeholder="yourname@email.com" id='email' className="w-100" />
+                </div>
+                <div className="mb-3">
+                    <label>Message</label>
+                <textarea name="" placeholder="Send me a message and I'll reply you as soon as possible..." id="" rows="5" className="w-100"></textarea>
+                </div>
+                <div className="d-flex mb-3 align-items-start"><input type="checkbox" className="mt-2 me-3" /> <span>You agree to providing your data to {name} who may contact you.</span></div>
+                <input type="submit" id="btn__submit" className="w-100 btn-contact" value="Send message"/>
+            </form>
         </section>
     )
 }
