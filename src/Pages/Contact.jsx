520f405def4db648d9458c801229bb9da3264b80
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-11-05 00:35:40 +0100

    Contact: added outline to inputs and also a function to button which when clicked will check if the message has any content and ask the user to input content

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,30 @@
+import { useState } from "react"
+
+const Result = () => {
+    return (<p style={{ color: "red", fontSize: '0.8rem' }}>Please enter a message</p>)
+}
 export default function Contact() {
-    const name="Priscilla"
+    const name = "Priscilla"
+    const [textEmpty, setEmptyText] = useState(false)
+    const [text, setText] = useState('')
+    const [classed, setClass] = useState('')
+    const handleChange = (e) => {
+        const newValue = e.target.value
+        setText(newValue)
+    }
+    const handleClick = (e) => {
+        e.preventDefault()
+        console.log("working")
+        if (text === '') {
+            setEmptyText(true)
+            setClass('text-input-outline')
+        }
+        else {
+            setEmptyText(false)
+            setClass('')
+            setText('')
+        }
+    }
     return (
         <section className="container container-contact px-3">
             <p className="h2">Contact Me</p>
@@ -21,10 +46,14 @@ export default function Contact() {
                 </div>
                 <div className="mb-3">
                     <label>Message</label>
-                <textarea name="" placeholder="Send me a message and I'll reply you as soon as possible..." id="" rows="5" className="w-100"></textarea>
+                    <textarea name="" placeholder="Send me a message and I'll reply you as soon as possible..." id="" rows="5" className={`${classed} w-100`} onChange={handleChange} value={text} required></textarea>
+                    <div>{textEmpty ? <Result /> : null}</div>
                 </div>
-                <div className="d-flex mb-3 align-items-start"><input type="checkbox" className="mt-2 me-3" /> <span>You agree to providing your data to {name} who may contact you.</span></div>
-                <input type="submit" id="btn__submit" className="w-100 btn-contact" value="Send message"/>
+                <div className="d-flex mb-3 align-items-start">
+                    <input type="checkbox" className="mt-2 me-3" />
+                    <label>You agree to providing your data to {name} who may contact you.</label>
+                </div>
+                <input type="submit" id="btn__submit" className="w-100 btn-contact" value="Send message" onClick={handleClick} />
             </form>
         </section>
     )
