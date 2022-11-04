d36296162d4abc0a40963450907f46d88258ed38
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-11-04 16:20:06 +0100

    Contact: wrote the structure of my Contact page and added ids to all elements. Linktree: Capitalize all first letter word and put the Contact Me button where it is supposed to be.

--- src/Components/Link.jsx
+++ src/Components/Link.jsx
@@ -5,13 +5,13 @@ export default function Links(){
     return(
         <>
         <div className="container">
-        <Link to="/contact" id="contact" className="btn btn-light w-100 py-3 mb-3 bold" >Contact me</Link>
         <a href="/" className="btn btn-light w-100 py-3 mb-3 bold" >Twitter</a>
-        <a href="https://training.zuri.team/" id="btn__zuri" className="btn btn-light w-100 py-3 mb-3 bold" >Zuri team</a>
-        <a href="http://books.zuri.team " id="books" className="btn btn-light w-100 py-3 mb-3 bold" >Zuri books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=priscillaegberi" id="book__python" className="btn btn-light w-100 py-3 mb-3 bold" >Python books</a>
-        <a href="https://background.zuri.team" id="pitch" className="btn btn-light w-100 py-3 mb-3 bold" >Background check for coders</a>
-        <a href="https://books.zuri.team/design-rules" id="book__design" className="btn btn-light w-100 py-3 mb-3 bold" >Design books</a>
+        <a href="https://training.zuri.team/" id="btn__zuri" className="btn btn-light w-100 py-3 mb-3 bold" >Zuri Team</a>
+        <a href="http://books.zuri.team " id="books" className="btn btn-light w-100 py-3 mb-3 bold" >Zuri Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=priscillaegberi" id="book__python" className="btn btn-light w-100 py-3 mb-3 bold" >Python Books</a>
+        <a href="https://background.zuri.team" id="pitch" className="btn btn-light w-100 py-3 mb-3 bold" >Background Check for Coders</a>
+        <a href="https://books.zuri.team/design-rules" id="book__design" className="btn btn-light w-100 py-3 mb-3 bold" >Design Books</a>
+        <Link to="/contact" id="contact" className="btn btn-light w-100 py-3 mb-3 bold" >Contact Me</Link>
         </div>
         <div className="d-flex justify-content-center mt-3 mb-5">
             <img src={img1} alt="" className="me-4" /> 
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,17 @@
 export default function Contact(){
+    const name="{name}"
     return(
-        <>
+        <section>
         <p>Contact Me</p>
-        
-        </>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
+        <form>
+            <input type="text" placeholder="Enter your first name" id='first_name' />
+            <input type="text" placeholder="Enter your last name" id='last_name'/>
+            <input type="email" placeholder="yourname@email.com" id='email'/>
+            <textarea name="" placeholder="Send me a message and I'll reply you as soon as possible..." id="" cols="30" rows="10"></textarea>
+            <input type="checkbox" /> <span>You agree to providing your data to {name} who may contact you.</span>
+            <button id="btn__submit">Send message</button>
+        </form>
+        </section>
     )
 }
