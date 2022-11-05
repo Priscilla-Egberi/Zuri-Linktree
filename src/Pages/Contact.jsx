0180de390fa103901de300a1a160f190202ae594
Priscilla-Egberi <priscillaegberi@gmail.com>
2022-11-05 02:00:46 +0100

    inputs: added required attributes on every input type-text

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -41,12 +41,13 @@ export default function Contact() {
             setEmailText('')
             setFirstNameText('')
             setLastNameText('')
+            // setCheck(true)
         }
     }
     const handleCheckChange = (e) => {
         if (e.target.checked) {
-           setCheck(false)
-           console.log('checked')
+            setCheck(false)
+            console.log('checked')
         }
         else {
             setCheck(true)
@@ -56,34 +57,39 @@ export default function Contact() {
     return (
         <section className="container container-contact px-3">
             <p className="h2">Contact Me</p>
-            <p className="mb-4 mt-4">Hi there, contact me to ask me about anything you have in mind.</p>
+            <p className="mb-5 mt-4">Hi there, contact me to ask me about anything you have in mind.</p>
             <form>
                 <div className="row mb-3">
                     <div className="col-12 col-md-6 mb-3 mb-md-0">
-                        <label>First name</label>
-                        <input type="text" placeholder="Enter your first name" onChange={handlefirstChange} value={firstNameText} id='first_name' className="w-100" />
+                        <label className="mb-2">First name</label>
+                        <input type="text" placeholder="Enter your first name" onChange={handlefirstChange} value={firstNameText} id='first_name' className="w-100" required />
                     </div>
                     <div className="col-12 col-md-6">
-                        <label>Last name</label>
-                        <input type="text" placeholder="Enter your last name" onChange={handlelastChange} value={lastNameText} id='last_name' className="w-100" />
+                        <label className="mb-2">Last name</label>
+                        <input type="text" placeholder="Enter your last name" onChange={handlelastChange} value={lastNameText} id='last_name' className="w-100" required />
                     </div>
                 </div>
                 <div className="mb-3">
-                    <label>Email</label>
-                    <input type="email" placeholder="yourname@email.com" onChange={handleemailChange} value={email} id='email' className="w-100" />
+                    <label className="mb-2">Email</label>
+                    <input type="email" placeholder="yourname@email.com" onChange={handleemailChange} value={email} id='email' className="w-100" required />
                 </div>
                 <div className="mb-3">
-                    <label>Message</label>
-                    <textarea name="" placeholder="Send me a message and I'll reply you as soon as possible..." id="" rows="5" className={`${classed} w-100`} onChange={handleChange} value={text} required></textarea>
+                    <label className="mb-2">Message</label>
+                    <textarea name="" placeholder="Send me a message and I'll reply you as soon as possible..." id="" rows="5"
+                        className={`${classed} w-100`}
+                        onChange={handleChange}
+                        value={text}
+                        required>
+                    </textarea>
                     <div>{textEmpty ? <Result /> : null}</div>
                 </div>
                 <div className="d-flex mb-3 align-items-start">
                     <input type="checkbox" className="mt-2 me-3" onChange={handleCheckChange} value={notCheck} />
                     <span>You agree to providing your data to {name} who may contact you.</span>
                 </div>
-                
-                {notCheck ? <input  type="submit" id="btn__submit" className="w-100 btn-contact" value="Send message" onClick={handleClick} disabled  /> : <input  type="submit" id="btn__submit" className="w-100 btn-contact" value="Send message" onClick={handleClick}  />};
-        
+
+                {notCheck ? <input type="submit" id="btn__submit" className="w-100 btn-contact" value="Send message" onClick={handleClick} disabled /> : <input type="submit" id="btn__submit" className="w-100 btn-contact" value="Send message" onClick={handleClick} />};
+
             </form>
         </section>
     )
